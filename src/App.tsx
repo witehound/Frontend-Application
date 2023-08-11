a4f3d0cd56c579ed5635d71c95843b1d38646982
witehound <netvid00@gmail.com>
2023-08-11 09:21:08 +0100

    set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>home</div>,
+  },
+  {
+    path: "product",
+    element: <div>pruduct page</div>,
+  },
+  {
+    path: "product/edit",
+    element: <div>edit</div>,
+  },
+]);
+
 function App() {
-  return <div className="">Hello World</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
